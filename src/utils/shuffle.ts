461bc5794442d3588a2bd2977881e7f8eb363398
Memnoc <smartdroidesign@gmail.com>
2024-05-02 18:09:37 +0100

    Changed generic T to Type as per best practices

--- src/utils/shuffle.ts
+++ src/utils/shuffle.ts
@@ -1,6 +1,6 @@
 // An Implementation of the Durstnfeld Shuffle algorithm
 
-export function shuffleArray<T>(items: T[]): T[] {
+export function shuffleArray<Type>(items: Type[]): Type[] {
   for (let i = items.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [items[i], items[j]] = [items[j], items[i]];
